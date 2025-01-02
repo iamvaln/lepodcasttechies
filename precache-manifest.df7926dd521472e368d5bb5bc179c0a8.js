376c37b8fed8872d94d3878e470d8698d9b208d4
Valentine Nguemne <nguemnev@gmail.com>
2025-01-02 18:02:44 +0100

    Updates

---
+++ precache-manifest.df7926dd521472e368d5bb5bc179c0a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebebd2962e8541a3d44bd0ef1a85231",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "dff8c01ec40f4b803648",
+    "url": "/repository-name/static/css/main.41eec723.chunk.css"
+  },
+  {
+    "revision": "9f6f87f316632486a068",
+    "url": "/repository-name/static/js/2.5d030c72.chunk.js"
+  },
+  {
+    "revision": "fc41f119e3413ff3b6d48c20b038ec68",
+    "url": "/repository-name/static/js/2.5d030c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dfeb3c052aef783630",
+    "url": "/repository-name/static/js/3.fe6a6dcc.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/repository-name/static/js/3.fe6a6dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a8c7f17eadb081d6f8",
+    "url": "/repository-name/static/js/4.a1dc6718.chunk.js"
+  },
+  {
+    "revision": "dff8c01ec40f4b803648",
+    "url": "/repository-name/static/js/main.126cfbed.chunk.js"
+  },
+  {
+    "revision": "11f01ca7b567f4cfbe87",
+    "url": "/repository-name/static/js/runtime-main.0e98b579.js"
+  }
+]);
